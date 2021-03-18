0d55749ba8fc806eab74321a4fb72f2225d8b9c3
markdrehmann <markdrehmann@gmail.com>
2021-03-18 17:17:26 -0500

    commit tree 4cdf03094244e292066f7a22e353cd3a8b6aaf52

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+// Create a variable called mod set to an equation that will calculate the remainder of dividing variable num3 by num4; the remainder should be 4.
+// Create a variable called max that finds the highest number in a set; the value returned should be 20.
 
-// Write your code here
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.ceil(Math.random()*10)
+
+let num3 = 20
+let num4 = 8
+let mod = num3 % num4
+
+let max = Math.max(1, 4, 20)
